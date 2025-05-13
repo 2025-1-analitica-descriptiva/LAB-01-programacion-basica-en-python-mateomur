486190be9e4bd2af2acf81ebe0be71bc151570b3
Mateo Murcia <mmurciav@unal.edu.co>
2025-05-13 15:31:04 -0500

    solucion pregunta 8

--- homework/pregunta_08.py
+++ homework/pregunta_08.py
@@ -5,25 +5,24 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_08():
-    """
-    Genere una lista de tuplas, donde el primer elemento de cada tupla
-    contiene  el valor de la segunda columna; la segunda parte de la tupla
-    es una lista con las letras (ordenadas y sin repetir letra) de la
-    primera  columna que aparecen asociadas a dicho valor de la segunda
-    columna.
+    agrupados = {}
 
-    Rta/
-    [(0, ['C']),
-     (1, ['B', 'E']),
-     (2, ['A', 'E']),
-     (3, ['A', 'B', 'D', 'E']),
-     (4, ['B', 'E']),
-     (5, ['B', 'C', 'D', 'E']),
-     (6, ['A', 'B', 'C', 'E']),
-     (7, ['A', 'C', 'D', 'E']),
-     (8, ['A', 'B', 'D', 'E']),
-     (9, ['A', 'B', 'C', 'E'])]
+    with open("files/input/data.csv", "r") as archivo:
+        for linea in archivo:
+            columnas = linea.strip().split('\t')
+            letra = columnas[0]               
+            numero = int(columnas[1])      
 
-    """
+            if numero in agrupados:
+                agrupados[numero].add(letra)
+            else:
+                agrupados[numero] = {letra}
+
+    resultado = []
+    for numero, letras in agrupados.items():
+        letras_ordenadas = sorted(letras)
+        resultado.append((numero, letras_ordenadas))
+
+    resultado.sort(key=lambda x: x[0])
+    return resultado
