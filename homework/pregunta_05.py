ddab005f14724359579b7af5f11d689c13743e79
Mateo Murcia <mmurciav@unal.edu.co>
2025-05-13 13:33:45 -0500

    Update pregunta_05.py

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -15,3 +15,25 @@ def pregunta_05():
     [('A', 9, 2), ('B', 9, 1), ('C', 9, 0), ('D', 8, 3), ('E', 9, 1)]
 
     """
+    with open('files/input/data.csv') as archivo:
+        max_min = {}
+        for line in archivo:
+            columnas = line.strip().split('\t')
+            if columnas:
+                letra = columnas[0]
+                valor = int(columnas[1])
+                if letra in max_min:
+                    max_min[letra][0] = max(max_min[letra][0], valor)
+                    max_min[letra][1] = min(max_min[letra][1], valor)
+                else:
+                    max_min[letra] = [valor, valor]
+    resultado = [(letra, max_valor, min_valor) for letra, (max_valor, min_valor) in max_min.items()]
+    resultado.sort(key=lambda x: x[0])
+    print(max_min)
+    return resultado
+
+
+if __name__ == "__main__":
+    print(pregunta_05())
+
+
